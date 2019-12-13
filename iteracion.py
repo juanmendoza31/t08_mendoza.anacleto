e880f9590d802ec426edf94592cbb941f319adb9
juanmendoza31 <58859048+juanmendoza31@users.noreply.github.com>
2019-12-13 13:13:16 -0500

    EJERCICIO

---
+++ iteracion.py
@@ -0,0 +1,74 @@
+#EJERCICIO 01
+msj="POMAPE"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 02
+msj="BIBLIOTECA"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 03
+msj="REFERENCIAS BIBLIOGRAFICAS"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 04
+msj="SAN LORENZO"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 05
+msj="TEORIA DE COLAS"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 06
+msj="ZONA SEGURA"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 07
+msj="COMPUTADORA"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 08
+msj="NO TODO LO QUE BRILLA ES ORO"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 09
+msj="EL MEJOR ESPEJO ES UN VIEJO AMIGO"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 10
+msj="DONDE HAY AMOR HAY VIDA"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 11
+msj="LA MUSICA ES AMOR"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 12
+msj="CUANDO NADA ES SEGURO, TODO ES POSIBLE"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 13
+msj="SE TU MISMO"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 14
+msj="EL AMOR LLEGA SIN SER VISTO"
+for letra in msj:
+    print(letra)
+
+#EJRCICIO 15
+msj="AMO LA VIDA"
+for letra in msj:
+    print(letra)
